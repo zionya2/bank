77cf595ab7e56ad5de21b2fe835443e62ca1d7fd
Alexander <zionya@ukr.net>
2021-10-25 18:17:07 +0300

    fixed bank

--- bank.js
+++ bank.js
@@ -18,18 +18,111 @@
 Создать отдельный git-репозиторий для этого проекта и дальше работать с этим проектом в этом репозитории.
 debit
 */
+/*
+accounts
+Debit account
+Credit account 
+current balance
+type
+expiration date
+
+Jacob
+William
+Ethan
+Michael
+
+A debit account has a current balance that is either positive or zero.
+A credit account has two balances: personal funds, credit funds and credit limit. 
+
+Each account has an activity, the date of activity when the expiration date of the plastic card expires.
+Each account has a currency type, UAH, RUB, USD, GBP, EUR and others.
+*/
 
 let baseClients = [
     {
         name: "John",
         surname: "Conor",
         isActive: true,
-        registration: 0,
-        
-        decardposition: 1,
-        salary: 200,
-        isFired: false, 
-    },    
+        registrationDate: "2014-02-01",
+        debitAccount: {
+            type: "USD",
+            balance: 500,
+            isActive: true,
+            registrationDate: "2019-02-01",
+            expirationDate: "2023-02-01",
+        },
+        creditAccaunt: {
+            type: "UAH",
+            balance: 0,
+            creditLimit: 300,
+            isActive: true,
+            registrationDate: "2020-02-01",
+            expirationDate: "2024-02-01",
+        }, 
+    },
+    {
+        name: "Jacob",
+        surname: "Onor",
+        isActive: true,
+        registrationDate: "2014-02-01",
+        debitAccount: {
+            type: "UAH",
+            balance: 1200,
+            isActive: true,
+            registrationDate: "2019-02-01",
+            expirationDate: "2022-02-01",
+        },
+        creditAccaunt: {
+            type: "USD",
+            balance: 200,
+            creditLimit: 300,
+            isActive: true,
+            registrationDate: "2020-02-01",
+            expirationDate: "2024-02-01",
+        }, 
+    },
+    {
+        name: "Eva",
+        surname: "Onor",
+        isActive: false,
+        registrationDate: "2014-02-01",
+        debitAccount: {
+            type: "EUR",
+            balance: 1200,
+            isActive: true,
+            registrationDate: "2019-02-01",
+            expirationDate: "2024-02-12",
+        },
+        creditAccaunt: {
+            type: "USD",
+            balance: 100,
+            creditLimit: 300,
+            isActive: true,
+            registrationDate: "2020-02-01",
+            expirationDate: "2024-02-01",
+        }, 
+    },
+    {
+        name: "William",
+        surname: "Onor",
+        isActive: true,
+        registrationDate: "2014-02-01",
+        debitAccount: {
+            type: "USD",
+            balance: 750,
+            isActive: true,
+            registrationDate: "2019-02-01",
+            expirationDate: "2025-02-01",
+        },
+        creditAccaunt: {
+            type: "EUR",
+            balance: 300,
+            creditLimit: 300,
+            isActive: true,
+            registrationDate: "2020-02-01",
+            expirationDate: "2024-02-01",
+        }, 
+    },
 ];
 
 class Bank {
@@ -39,14 +132,118 @@ class Bank {
         this.#clients = clients;
     }
 
+    /*async getCurrencyRates(){
+        let url = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
+        let json;
+        let response = await fetch(url);
+        let result = {};
+
+        if (response.ok) {
+            json = await response.json();
+        }
+
+        for (let i = 0; i < json.length; i++){
+            result[json[i]["ccy"]] = Number(json[i]["sale"]);
+        }
+
+        return result;
+    }*/
+
+    getCurrencyRates(){
+        let url = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
+        //let json;
+        //let response = await fetch(url);
+        //let result = {};
+
+        let result = fetch(url)
+            .then((result) => result.json())
+            .then((obj) => {
+                    let result = {};
+                    for (let i = 0; i < obj.length; i++){
+                        result[obj[i]["ccy"]] = Number(obj[i]["sale"]);
+                    }
+                    return result;
+                }
+            );
+        return result;
+    }
+
+    countTotalMoneyInsideBank(type) {
+        let rates = this.getCurrencyRates();
+        //rates
+        console.log(rates.USD, "rates", rates, rates["USD"]);
+        let totalMoney = {};        
+
+        for (let i = 0; i < this.#clients.length; i++) {
+
+            if (totalMoney[this.#clients[i].debitAccount.type] === undefined || totalMoney[this.#clients[i].creditAccaunt.type] === undefined) {
+                totalMoney[this.#clients[i].debitAccount.type] = 0;
+                totalMoney[this.#clients[i].creditAccaunt.type] = 0;
+            };
+            totalMoney[this.#clients[i].debitAccount.type] += this.#clients[i].debitAccount.balance;
+            totalMoney[this.#clients[i].creditAccaunt.type] += (this.#clients[i].creditAccaunt.balance - this.#clients[i].creditAccaunt.creditLimit);
+        }
+        console.log("totalManey", totalMoney)        
+        for (let elem in totalMoney) {
+            if (elem !== type.toUpperCase()) {
+                if (elem !== "UAH") {
+                    console.log("rates[elem]", rates["USD"], elem)
+                    totalMoney["UAH"] += totalMoney[elem] * rates[elem];
+                }
+            }
+        }
+        
+        console.log("totalManeypo", totalMoney);
+
+        return Math.floor((totalMoney["UAH"] / rates[type.toUpperCase()] + totalMoney[type.toUpperCase()]) * 100) / 100;
+    }
+
+    async countOweMoneyAllClients(type) {
+        let rates = await this.getCurrencyRates();
+        let totalMoney = {};
+
+        for (let i = 0; i < this.#clients.length; i++) {
+
+            if (totalMoney[this.#clients[i].creditAccaunt.type] === undefined) {
+                totalMoney[this.#clients[i].creditAccaunt.type] = 0;
+            }
+            totalMoney[this.#clients[i].creditAccaunt.type] += (this.#clients[i].creditAccaunt.creditLimit - this.#clients[i].creditAccaunt.balance);
+        }
+        
+        for (let elem in totalMoney) {
+            if (elem !== type.toUpperCase()) {
+                if (elem !== "UAH") {
+                    totalMoney["UAH"] += totalMoney[elem] * rates[elem];
+                }
+            }
+        }
+        return Math.floor((totalMoney["UAH"] / rates[type.toUpperCase()] + totalMoney[type.toUpperCase()]) * 100) / 100;
+    }
+
+    countDebtorsTotalDebt(isActive){
+        let result = { debtor: 0,};
+
+        for (let i = 0; i < this.#clients.length; i++) {
+
+            if (this.#clients[i].isActive === isActive) {
+                
+                if (this.#clients[i].creditAccaunt.creditLimit !== this.#clients[i].creditAccaunt.balance){
+                    result.debtor++;
+                    
+                    if (result[this.#clients[i].creditAccaunt.type] === undefined) {
+                        result[this.#clients[i].creditAccaunt.type] = 0;
+                    };
+                    result[this.#clients[i].creditAccaunt.type] += (this.#clients[i].creditAccaunt.creditLimit - this.#clients[i].creditAccaunt.balance);
+                }    
+            }
+        }
+        return result;
+    } 
 
 }
 
 let bank = new Bank(baseClients);
 
-//console.log(res.findMinSalaryPosition("head"))
-//console.log(res.avarageSalaryDepartment("administration"))
-//console.log(res.countSalary());
-//console.log(res.countFierd());
-//console.log(res.findHead(true));
-
+console.log("rt", bank.countTotalMoneyInsideBank("EUR"));
+//console.log("rt", bank.countOweMoneyAllClients("USD"));
+//console.log("rt", bank.countDebtorsTotalDebt(true));
